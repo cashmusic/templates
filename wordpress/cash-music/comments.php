dc6adbe1542fe2be356d61a538f12745321ca98a
Chris Leckie <me@christopherleckie.com>
2016-07-13 16:31:36 -0700

    i think i can i think i can
    Passing Wordpress Theme Tests

--- wordpress/cash-music/comments.php
+++ wordpress/cash-music/comments.php
@@ -22,7 +22,7 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'cashmusic' ),
+				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'cash-music' ),
 					number_format_i18n( get_comments_number() ), get_the_title() );
 			?>
 		</h2>
@@ -45,7 +45,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'cashmusic' ); ?></p>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'cash-music' ); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>
