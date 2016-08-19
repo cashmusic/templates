3bf1a4c80af5fecc8af36ae580e001cefb15481b
Chris Leckie <me@christopherleckie.com>
2016-08-19 15:06:45 -0700

    Northern Lights style coming together

--- HTML Boilerplate/js/main.js
+++ HTML Boilerplate/js/main.js
@@ -3,11 +3,13 @@
 document.addEventListener("DOMContentLoaded", function(e) {
 
   var mainNav = document.getElementById('nav');
+  var docBody = document.body;
   var navToggle = document.getElementById('menutoggle');
 
   // Establish a function to toggle the class "collapse"
   function mainNavToggle() {
       mainNav.classList.toggle('collapsed');
+      docBody.classList.toggle('fullwidth');
   }
 
   // Add a click event to run the mainNavToggle function
